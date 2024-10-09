26bdbdae558aae7152514339cf75a8299de3be3a
asinha20012002 <sinhaayush933@gmail.com>
2024-10-09 14:12:47 +0530

    changes by ayush

---
+++ src/com/flipfit/bean/BookedSlot.java
@@ -0,0 +1,40 @@
+package com.flipfit.bean;
+
+public class BookedSlot {
+    private int id;
+    private int gymCentreId;
+    private int slotId;
+    private String custEmail;
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getGymCentreId() {
+        return gymCentreId;
+    }
+    public void setGymCentreId(int gymCentreId) {
+        this.gymCentreId = gymCentreId;
+    }
+    public int getSlotId() {
+        return slotId;
+    }
+    public void setSlotId(int slotId) {
+        this.slotId = slotId;
+    }
+    public String getCustEmail() {
+        return custEmail;
+    }
+    public void setCustEmail(String custEmail) {
+        this.custEmail = custEmail;
+    }
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
